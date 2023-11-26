09464a32b479a652e5f5645962c9f69d930224eb
LGsus123 <colonia0503@gmail.com>
2023-11-26 13:51:20 -0500

    first commit, Se crean las tablas desde el codigo

---
+++ src/main/java/com/sistema/examenes/entidades/Rol.java
@@ -0,0 +1,51 @@
+package com.sistema.examenes.entidades;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ROLES")
+public class Rol {
+
+	@Id
+	private Long rolId;
+	private String nombre;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,mappedBy = "rol")
+	private Set<UsuarioRol> usuariosRoles = new HashSet<>();
+
+	public Long getRolId() {
+		return rolId;
+	}
+
+	public void setRolId(Long rolId) {
+		this.rolId = rolId;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Set<UsuarioRol> getUsuariosRoles() {
+		return usuariosRoles;
+	}
+
+	public void setUsuariosRoles(Set<UsuarioRol> usuariosRoles) {
+		this.usuariosRoles = usuariosRoles;
+	}
+	
+	public Rol() {
+		
+	}
+}
---
+++ src/main/java/com/sistema/examenes/entidades/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package com.sistema.examenes.entidades;
